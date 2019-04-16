b6c03f98461a155f6d6eff6a53d8ea7c7a4d339c
Grant Fanian <grantfanian@gmail.com>
2019-04-16 18:48:03 +0300

    main commit

---
+++ onetomult.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+import argparse, os, subprocess, fnmatch,sys,platform
+arg=argparse.ArgumentParser()
+arg.add_argument("dir", type=str, help="Directory that contains target files")
+arg.add_argument("src",type=str, help="Source file")
+arg.add_argument("--mask", "-m", type=str, help='Mask to choose files from')
+args=arg.parse_args()
+if args.mask!=None and args.mask!="" and args.mask!=" ":
+    mask=args.mask
+else:
+    mask=None
+onlyfiles = [f for f in os.listdir(args.dir) if os.path.isfile(os.path.join(args.dir, f))]
+filtered=[]
+if mask!=None:
+    for file in os.listdir('.'):
+        if fnmatch.fnmatch(file, mask):
+            filtered.append(file)
+
+else:
+    filtered=onlyfiles
+commandout=[]
+if platform.system=="Windows":
+    for i in range(0,len(filtered)-1):
+        a=list(subprocess.getstatusoutput("copy /F "+args.src+" "+filtered[i]))
+        if a[0]!=0:
+            print(a[1])
+            raise UserWarning("Error occurred.\n Abort, Retry or Skip? ")
+            userinput=input()
+            if userinput=="A" or userinput=="a":
+                sys.exit(1)
+            if userinput=="S" or userinput=="s":
+                i+=1
+        else:
+            i+=1
+        commandout.append(a)
+else:
+    for i in range(0,len(filtered)-1):
+        a=list(subprocess.getstatusoutput("cp -vf "+args.src+" "+filtered[i]))
+        if a[0]!=0:
+            print(a[1])
+            raise UserWarning("Error occurred.\n Abort, Retry or Skip? ")
+            userinput=input()
+            if userinput=="A" or userinput=="a":
+                sys.exit(1)
+            if userinput=="S" or userinput=="s":
+                i+=1
+        else:
+            i+=1
+    commandout.append(a)
